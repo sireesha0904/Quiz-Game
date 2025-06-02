c7327fd35ac50b08b2efe122d045773ed887efe8
Modi Sireesha <modisireesha09@gmail.com>
2025-06-02 16:02:52 +0530

    Quiz Game

--- QuizGame/src/db/DBConnection.java
+++ QuizGame/src/db/DBConnection.java
@@ -1,5 +1,30 @@
 package db;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/online_quiz_app";
+    private static final String USER = "your_mysql_username";
+    private static final String PASSWORD = "your_mysql_password";
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
